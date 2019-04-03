1372086e7338ffb03672932eba9265dcfce4132e
Salmasghaier <salmasghaier93@gmail.com>
2019-04-03 11:43:38 +0200

    First deployment of guincorne REST API

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,13 @@
 # wsgi.py
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+PRODUCTS = [
+    { 'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' }
+]
+@app.route('/api/v1/products')
+def products():
+    return jsonify (products)
+
