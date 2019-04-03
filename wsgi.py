4e87d991f2dfbc4c056ea1fc8b6e6b88d4ddd7c9
Salmasghaier <salmasghaier93@gmail.com>
2019-04-03 12:22:14 +0200

    add Falsk-READ feature

--- wsgi.py
+++ wsgi.py
@@ -3,10 +3,17 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
+
 PRODUCTS = [
     { 'id': 1, 'name': 'Skello' },
-    { 'id': 2, 'name': 'Socialive.tv' }
+    { 'id': 2, 'name': 'Socialive.tv' },
+    { 'id': 3, 'name': 'third.tv' }
 ]
+
+@app.route('/')
+def hello():
+    return "Hello World!"
 
 @app.route('/api/v1/products')
 def products():
